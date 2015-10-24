1422bf94e5743890d6a153e04ad68ece83c9592f
root <root@a72cf09c-9a6f-4b4f-a78e-fc506195e0aa.cs547dcloud.internal>
2015-10-24 18:35:58 +0900

    add count

--- TextServer/src/ChatServer.java
+++ TextServer/src/ChatServer.java
@@ -9,9 +9,10 @@ public class ChatServer {
 	//서버소켓 - 클라이언트의 접속을 받아들이는 클래스
 	int port=8080;
 	ServerSocket server;
-	
 	Vector<ServerThread> connectList;
-	
+
+	int count = 1;	
+
 	public ChatServer() {
 		connectList = new Vector<ServerThread>();
 		
@@ -25,7 +26,7 @@ public class ChatServer {
 				Socket client = server.accept();
 				InetAddress inet = client.getInetAddress();
 				String ip = inet.getHostAddress();
-				System.out.println(ip+"-접속자 발견");
+				System.out.println(count + " ) " + ip + " - 접속자 발견");
 				
 				//대화용 쓰레드 생성 및 소켓
 				ServerThread serverThread = new ServerThread(connectList, client);
